421fb26bc67e0d484a050100780fe2114f93022e
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-11-16 18:32:53 +0100

    Updates the appearance of the side navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  clock = Observable
+        .interval(1000)
+        .map(() => new Date());
 }
